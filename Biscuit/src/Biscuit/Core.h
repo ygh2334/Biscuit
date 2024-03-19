78c247c4ae42da493468d6aea7e56f5d25c68152
guanhua yao <ygh2334@awzg.office.gy>
2024-03-19 22:50:20 +0800

    feat：完成imgui events

--- Biscuit/src/Biscuit/Core.h
+++ Biscuit/src/Biscuit/Core.h
@@ -20,3 +20,4 @@
 
 #define BIT(x) (1 << x)	
 
+#define BC_BIND_EVENT_FN(fn) std::bind(&fn,this,std::placeholders::_1)
--- Biscuit/src/Biscuit/ImGui/ImGuiLayer.h
+++ Biscuit/src/Biscuit/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "Biscuit/Layer.h"
+#include "Biscuit/Events/KeyEvent.h"
+#include "Biscuit/Events/MouseEvent.h"
+#include "Biscuit/Events/ApplicationEvent.h"
 
 namespace Biscuit
 {
@@ -14,6 +17,15 @@ namespace Biscuit
 		void OnDetach();
 		void OnUpdate();
 		void OnEvent(Event& event);
+	private:
+		bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& e);
+		bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& e);
+		bool OnMouseMovedEvent(MouseMovedEvent& e);
+		bool OnMouseScrolledEvent(MouseScrolledEvent& e);
+		bool OnKeyPressedEvent(KeyPressedEvent& e);
+		bool OnKeyReleasedEvent(KeyReleasedEvent& e);
+		bool OnKeyTypedEvent(KeyTypedEvent& e);
+		bool OnWindowsResizeEvent(WindowResizeEvent& e);
 	private:
 		float m_Time = 0.0f;
 
