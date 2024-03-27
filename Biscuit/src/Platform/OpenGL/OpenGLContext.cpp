68e22d5e6df5a07d108198132996904d9b2ac63d
guanhua yao <ygh2334@awzg.office.gy>
2024-03-27 23:30:47 +0800

    feat:绘制第一个三角形

--- Biscuit/src/Platform/OpenGL/OpenGLContext.cpp
+++ Biscuit/src/Platform/OpenGL/OpenGLContext.cpp
@@ -18,6 +18,12 @@ namespace Biscuit
 		glfwMakeContextCurrent(m_WindowHandle);
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		BC_CORE_ASSERT(status, "Failed to initialize Glad!");
+
+		BC_CORE_INFO("OpenGL Info:");
+		BC_CORE_INFO("  Vendor: {0}", reinterpret_cast<const char*>(glGetString(GL_VENDOR)));
+		BC_CORE_INFO("  Renderer: {0}", (const char*)(glGetString(GL_RENDERER)));
+		BC_CORE_INFO("  Version: {0}", reinterpret_cast<const char*>(glGetString(GL_VERSION)));
+
 	}
 
 	void OpenGLContext::SwapBuffers()
