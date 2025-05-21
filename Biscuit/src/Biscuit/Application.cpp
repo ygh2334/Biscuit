13cd54e554e6e3997827e38e8f51d0f3b785e814
guanhua yao <ygh2334@awzg.office.gy>
2025-05-21 23:02:18 +0800

    learn: 35节 (Creating an Orthographic Camera) 完成  5/21/2025

--- Biscuit/src/Biscuit/Application.cpp
+++ Biscuit/src/Biscuit/Application.cpp
@@ -170,15 +170,14 @@ namespace Biscuit {
 			RenderCommand::SetClearColor({0.1f,0.1f,0.1f,1});
 			RenderCommand::Clear();
 
-			Renderer::BeginScene();
 
-			m_BlueShader->Bind();
-			m_BlueShader->UploadUniformMat4("u_ViewProjection", m_Camera.GetViewProjectionMatrix());
-			Renderer::Submit(m_SquareVA);
+			m_Camera.SetPosition({ 0.5f, 0.5f, 0.0f });
+			m_Camera.SetRotation(45.0f);
 
-			m_Shader->Bind();
-			m_Shader->UploadUniformMat4("u_ViewProjection", m_Camera.GetViewProjectionMatrix());
-			Renderer::Submit(m_VertexArray);
+			Renderer::BeginScene(m_Camera);
+
+			Renderer::Submit(m_BlueShader, m_SquareVA);
+			Renderer::Submit(m_Shader, m_VertexArray);
 
 			Renderer::EndScene();
 
